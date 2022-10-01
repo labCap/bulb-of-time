6074a2ac3478da603108ada9e0a1b0e92185691b
LabCap <banlazthosow@gmail.com>
2022-10-01 21:16:48 +0300

    updated HTML structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { Header } from "./components/header";
+
 function App() {
-	return <div className="App">hi </div>;
+	return (
+		<div className="wrapper">
+			<Header />
+			<section className="app">
+				<div className="app__container">hi</div>
+			</section>
+		</div>
+	);
 }
 
 export default App;
