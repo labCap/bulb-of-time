8a502fc01cf9a69f816799e7295e5b2d7378d98a
LabCap <banlazthosow@gmail.com>
2022-10-02 18:29:37 +0300

    added a banner when the message list is empty

--- src/components/rating/rating.jsx
+++ src/components/rating/rating.jsx
@@ -2,21 +2,18 @@ import React from "react";
 import { RatingRow } from "../../components/rating-row";
 
 import "./rating.scss";
-import "../rating-row/rating-row.scss";
 
 const LIST = [
 	{
-		id: 1,
+		id: "1",
 		mess: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit dolore eaque id culpa officiis a, ipsam minus pariatur quasi modi nihil sequi soluta ullam ab consectetur cupiditate laborum corporis dignissimos!",
 		name: "Danil",
 	},
 	{
-		id: 2,
+		id: "2",
 		mess: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
 		name: "Adimin",
 	},
-
-	// 1, 2, 3,
 ];
 
 export const Rating = () => {
@@ -28,15 +25,20 @@ export const Rating = () => {
 						<h1>Цей список де ти можеш залишити щось на згадку</h1>
 					</div>
 					<div className="rating__body">
-						<table className="rating__table">
-							{LIST.map((list) => (
-								<RatingRow
-									num={list.id}
-									mess={list.mess}
-									name={list.name}
-								/>
-							))}
-						</table>
+						{LIST.length > 0 ? (
+							<table className="rating__table">
+								{LIST.map((list) => (
+									<RatingRow
+										key={list.id}
+										num={list.id}
+										mess={list.mess}
+										name={list.name}
+									/>
+								))}
+							</table>
+						) : (
+							<div className="rating__info">Нічого немає</div>
+						)}
 					</div>
 				</div>
 			</div>
